f2b04b494f05d376a6e0adbf42180ffe856a6171
dylan1496 <dachaconp@unal.edu.co>
2021-04-06 17:35:56 -0500

    global emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import { Global, css } from '@emotion/react';
 
 const Layout = ({children}) => {
     return (
         <>
+            <Global
+                styles={css`
+                html {
+                    font-size: 62.5%;
+                    box-sizing: border-box;
+                }
+                *, *:before, *:after {
+                    box-sizing: inherit;
+                }
+                body {
+                    font-size: 1.6rem;
+                    line-height: 2;
+                    font-family: 'Lato', sans-serif;
+                }
+                h1, h2, h3 {
+                    margin: 0;
+                    line-height: 1.5;
+                }
+                h1, h2 {
+                    text-align: center;
+                    font-family: 'Lato', sans-serif;
+                    font-weight: 300;
+                }
+                h3 {
+                    font-family: 'Roboto', sans-serif;
+                }
+                ul {
+                    list-style: none;c 
+                    margin: 0;
+                    padding: 0;
+                }
+                .contenedor {
+                    max-width: 120rem;
+                    margin: 0 auto;
+                    width: 95%;
+                }
+                img {
+                    max-width: 100%;
+                }
+
+                `}
+            /> 
+
             <Helmet>
                 <title>Bienes Raices Gatsby</title>
                 <meta name="description" content="Sitio web de bienes raíces en Gatsby" />
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" crossorigin="anonymous" />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"  integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU=" crossorigin="anonymous" />
                 <link href="https://fonts.googleapis.com/css?family=Lato300,400,700|Roboto:400,700&display=swap" rel="stylesheet" />
             </Helmet>
             {children}
